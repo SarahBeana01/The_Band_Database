dc076fa2913cf404f11295189005426a6c5979ee
SarahBeana01 <dunns10@ferris.edu>
2023-10-11 21:04:00 -0400

    Version 5.7

--- app/src/main/java/com/zybooks/thebanddatabase/MainActivity.java
+++ app/src/main/java/com/zybooks/thebanddatabase/MainActivity.java
@@ -6,13 +6,22 @@ import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //find the screen width
+        Configuration configuration = this.getResources().getConfiguration();
+        int screenWidthDP = configuration.screenWidthDp;
+
+        Log.d("MainActivity", "Screen Width:" + String.valueOf(screenWidthDP));
 
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.nav_host_fragment);
